385b2f087f065b80229e4e2f89cd916c5b85c47b
OCD0711 <conghaohu0711@outlook.com>
2018-12-12 14:07:25 +0800

    添加网页标题崩溃欺骗搞怪特效

---
+++ themes/next/source/js/src/crash_cheat.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "http://q2.qlogo.cn/headimg_dl?bs=870321017&dst_uin=870321017&dst_uin=870321017&;dst_uin=870321017&spec=100&url_enc=0&referer=bu_interface&term_type=PC");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "http://q2.qlogo.cn/headimg_dl?bs=870321017&dst_uin=870321017&dst_uin=870321017&;dst_uin=870321017&spec=100&url_enc=0&referer=bu_interface&term_type=PC");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
